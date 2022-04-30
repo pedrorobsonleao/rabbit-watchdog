c6c9e0bf289a1739ee7dae9f2b9a0572270661ad
Pedro Robson Leão <pedro.leao@neon.com.br>
2022-04-30 07:23:32 -0300

    set silent parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@ function main() {
  [ ! -z "${RABBIT_PWD}" ] && \
  [ ! -z "${SLACK_URL}" ] && \
  [ ! -z "${SLACK_CHANNEL}" ] && {
-    yarn start  \
+    yarn --silent start  \
     --url           "${RABBIT_URL}" \
     --user          "${RABBIT_USER}" \
     --pass          "${RABBIT_PWD}" \
     --slack_url     "${SLACK_URL}" \
     --slack_channel "${SLACK_CHANNEL}"
   } || {
-      yarn start ${@};
+      yarn --silent start ${@};
   }
 }
 
